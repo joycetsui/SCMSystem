4359467e9456e5ba873375c6440b6b520b9c9f22
xwnage <x272liu@uwaterloo.com>
2016-07-18 13:16:07 -0400

    committing to rebase

---
+++ SCM Desktop Application/access.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCM_Desktop_Application
+{
+    class access
+    {
+
+        public static string cnStr;
+    }
+}
