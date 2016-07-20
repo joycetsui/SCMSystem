3b5ec0017988a12f08f4e5e94b8fa39f9aa55656
joycetsui <joyce_tsui@hotmail.com>
2016-07-19 21:03:16 -0400

    Changes for shipping and supplier to access database

---
+++ DataAccess/Shipping.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccess
+{
+    class Shipping
+    {
+        public static DataTable getShippingCompanies()
+        {
+            string query = "SELECT [Company Name], [Contact Info], [Standard Shipping Rate] " +
+                            "FROM [Shipping Company];";
+            
+            return Database.executeSelectQuery(query);
+        }
+
+        public static void updateShippingCompany(int id, String companyName, String contactInfo, double rate)
+        {
+            string query = "update [Shipping Company] " +
+                           "set [Company Name] = @companyName, " +
+                               "[Contact Info] = @contactInfo, " +
+                               "[Standard Shipping Rate] = @rate " +
+                           "where [Shipping Company ID]= @id";
+
+            List<SqlParameter> pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("companyName", companyName));
+            pars.Add(new SqlParameter("contactInfo", contactInfo));
+            pars.Add(new SqlParameter("rate", rate));
+            pars.Add(new SqlParameter("id", id));
+
+            Database.executeInsertUpdateQuery(query, pars);
+        }
+
+        public static void addShippingCompany(int id, String companyName, String contactInfo, double rate)
+        {
+            String query = "insert into [Shipping Company]([Shipping Company ID],[Company Name], [Contact Info],[Standard Shipping Rate]) " +
+                            "values( @id, @companyName, @contactInfo, @rate) " +
+                            "SELECT scope_identity();";
+
+            List<SqlParameter> pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("companyName", companyName));
+            pars.Add(new SqlParameter("contactInfo", contactInfo));
+            pars.Add(new SqlParameter("rate", rate));
+            pars.Add(new SqlParameter("id", id));
+
+            Database.executeInsertUpdateQuery(query, pars);
+        }
+
+        public static void deleteShippingCompany(int id)
+        {
+            string query = "delete from [Shipping Company] where [Shipping Company ID]= @id";
+
+            List<SqlParameter> pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("id", id));
+
+            Database.executeInsertUpdateQuery(query, pars);
+        }
+
+    }
+}
---
+++ DataAccess/Suppliers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Suppliers
+    {
+        public static DataTable getSupplier()
+        {
+            string query = "SELECT [Company Name], [Location], [Payment Details],  " +
+                            "FROM [Suppliers];";
+
+            return Database.executeSelectQuery(query);
+        }
+
+        public static void updateSupplier(int id, String companyName, String location, String paymentDetails)
+        {
+            string query = "update [Suppliers] " +
+                           "set [Company Name] = @companyName, " +
+                               "[Location] = @location, " +
+                               "[Payment Details] = @paymentDetails " +
+                           "where [Supplier ID]= @id";
+
+            List<SqlParameter> pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("companyName", companyName));
+            pars.Add(new SqlParameter("location", location));
+            pars.Add(new SqlParameter("paymentDetails", paymentDetails));
+            pars.Add(new SqlParameter("id", id));
+
+            Database.executeInsertUpdateQuery(query, pars);
+        }
+
+        public static void addSupplier(String companyName, String location, String paymentDetails)
+        {
+            String query = "insert into [Suppliers]([Company Name], [Location],[Payment Details]) " +
+                            "values( @id, @companyName, @location, @paymentDetails) " +
+                            "SELECT scope_identity();";
+
+            List<SqlParameter> pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("companyName", companyName));
+            pars.Add(new SqlParameter("location", location));
+            pars.Add(new SqlParameter("paymentDetails", paymentDetails));
+
+            Database.executeInsertUpdateQuery(query, pars);
+        }
+
+        public static void deleteSupplier(int id)
+        {
+            string query = "delete from [Suppliers] where [Suppilier ID]= @id";
+
+            List<SqlParameter> pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("id", id));
+
+            Database.executeInsertUpdateQuery(query, pars);
+        }
+    }
+}
